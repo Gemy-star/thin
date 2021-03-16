8d2e1ef4e7bf08033f5ef9d237fb7a97a4bb3015
Mohamed Gamal <Mohamed.Gamal@arabiansystems.com>
2021-03-16 23:24:08 +0200

    final

--- devices/urls.py
+++ devices/urls.py
@@ -9,11 +9,17 @@ urlpatterns = [
     path('search/clients/<int:pk>', views.thin_uint, name='thin_uint'),
     path('reports/today', views.ClientsTodayReport.as_view(), name='report-today'),
     path('reports/store', views.ClientsAllReport.as_view(), name='report-store'),
+    path('test', views.test, name='test'),
     path('reports/unit', views.report, name='report-sara'),
     path('sara/grid', views.sara_grid, name='sara-grid'),
     path('delete/thin', views.delete_thin, name='delete_thin'),
     path('get/thin', views.get_thin_byId, name='get_thin'),
     path('update/thin', views.update_thin, name='update_thin'),
     path('create/thin', views.create_thin, name='create_thin'),
+    path('report/main', views.MainReport, name='main_report'),
+    path('reportdatemain', views.MainReport, name='reportdatemain_report'),
+    path('maingrid', views.mainGrid, name='maingrid'),
+    path('update/main', views.update_main, name='update_main'),
+    path('create/main', views.create_main, name='create_main'),
 
 ]
--- devices/views.py
+++ devices/views.py
@@ -9,12 +9,15 @@ from django.views.generic import View
 from django.http import HttpResponse
 from django.core import serializers
 from django.http import JsonResponse
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.contrib.auth.decorators import login_required
 
 
 def home_dash(request):
     return render(request, 'devices/home-dash.html')
 
 
+@login_required(login_url='login')
 def add_device(request):
     if request.method == "GET":
         return render(request, 'devices/add_device.html')
@@ -38,6 +41,7 @@ def add_device(request):
             return redirect('add_device')
 
 
+@login_required(login_url='login')
 def search_clients(request):
     clients = models.ThinDevicesUnits.objects.all()
     clients_filter = ThinClientFilter(request.GET, queryset=clients)
@@ -50,6 +54,7 @@ def work_flow(request):
     return render(request, 'devices/work_flow.html')
 
 
+@login_required(login_url='login')
 def thin_uint(request, pk):
     thin = models.ThinDevicesUnits.objects.get(pk=pk)
     context = {"thin": thin}
@@ -65,7 +70,7 @@ def thin_uint(request, pk):
 class ClientsAllReport(View):
     def get(self, request, *args, **kwargs):
         template = get_template('allclients_reports.html')
-        units = models.ThinDevicesUnits.objects.filter(status=1)
+        units = models.ThinDevicesUnits.objects.filter(status='مخزن')
         user_obj = User.objects.get(pk=request.user.pk)
         context = {
             "company": "مركز النظم و الدعم المتكامل",
@@ -114,18 +119,23 @@ class ClientsTodayReport(View):
         return HttpResponse("Not found")
 
 
+@login_required(login_url='login')
+def test(request):
+    return render(request, 'devices/test.html')
+
+
 def report(request):
     if request.method == "POST":
         status = request.POST.get('status')
         name = request.POST.get('name')
         template = get_template('uint_reports.html')
-        units = models.ThinDevicesUnits.objects.filter(name__contains=name, status=int(status))
+        units = models.ThinDevicesUnits.objects.filter(name__contains=name, status=status)
         user_obj = User.objects.get(pk=request.user.pk)
         context = {
             "company": "مركز النظم و الدعم المتكامل",
             "user": user_obj,
             "devices": units,
-            "topic": "تفاصيل العمل اليومى  ",
+            "topic": "تفاصيل العمل اليومى للوحدة  ",
             "today": datetime.today().strftime('%Y-%m-%d'),
         }
         html = template.render(context)
@@ -140,14 +150,7 @@ def report(request):
             response['Content-Disposition'] = content
             return response
         return HttpResponse("Not found")
-    return render(request, 'devices/thinclients.html')
-
-
-def sara_grid(request):
-    qs = models.ThinDevicesUnits.objects.all()
-    clients_filter = ThinClientFilter(request.GET, queryset=qs)
-    thins = clients_filter.qs
-    return render(request, 'devices/device.html', context={"data": thins})
+    return render(request, 'devices/filterreport.html')
 
 
 def delete_thin(request):
@@ -169,6 +172,7 @@ def get_thin_byId(request):
 def update_thin(request):
     if request.method == "POST" and request.is_ajax:
         update_id = request.POST.get('update_id')
+        user = User.objects.get(pk=request.user.pk)
         name = request.POST.get('name')
         status = request.POST.get('status')
         cdb = request.POST.get('cdb')
@@ -179,6 +183,7 @@ def update_thin(request):
         devices_done = request.POST.get('devices_done')
         total_devices = request.POST.get('total_devices')
         thin = models.ThinDevicesUnits.objects.get(pk=update_id)
+        thin.added_by = user
         thin.name = name
         thin.status = status
         thin.cdb = cdb
@@ -188,11 +193,13 @@ def update_thin(request):
         thin.database_zone = database_zone
         thin.devices_done = devices_done
         thin.total_devices = total_devices
+        thin.save()
         return JsonResponse({"data": 1})
 
 
 def create_thin(request):
     if request.method == "POST" and request.is_ajax:
+        user_obj = User.objects.get(pk=request.user.pk)
         name = request.POST.get('name')
         status = request.POST.get('status')
         cdb = request.POST.get('cdb')
@@ -202,7 +209,8 @@ def create_thin(request):
         database_name = request.POST.get('database_name')
         devices_done = request.POST.get('devices_done')
         total_devices = request.POST.get('total_devices')
-        thin = models.ThinDevicesUnits(name=name, status=int(status), cdb=cdb, epg=epg, short_name=short_name,
+        thin = models.ThinDevicesUnits(added_by=user_obj, name=name, status=int(status), cdb=cdb, epg=epg,
+                                       short_name=short_name,
                                        database_name=database_name
                                        , database_zone=database_zone, devices_done=devices_done,
                                        total_devices=total_devices)
@@ -211,3 +219,115 @@ def create_thin(request):
             return JsonResponse({"data": 1})
         else:
             return JsonResponse({"data": -1})
+
+
+def mainGrid(request):
+    uts = models.ThinDevicesUnits.NAME_CHOICES
+    model = models.ThinDevicesUnits.objects.all()
+    clients_filter = ThinClientFilter(request.GET, queryset=model)
+    thins = clients_filter.qs
+    page = request.GET.get('page', 1)
+    paginator = Paginator(thins, 8)
+    try:
+        thins = paginator.page(page)
+    except PageNotAnInteger:
+        thins = paginator.page(1)
+    except EmptyPage:
+        thins = paginator.page(paginator.num_pages)
+    return render(request, 'devices/maingrid.html', context={"data": thins, 'units': uts})
+
+
+def create_main(request):
+    if request.method == "POST" and request.is_ajax:
+        name = request.POST.get('name')
+        build = request.POST.get('build')
+        groupwork = request.POST.get('groupwork')
+        twinte_five_percentgy = request.POST.get('twinte_five_percentgy')
+        fifty_percentgy = request.POST.get('fifty_percentgy')
+        catoperation = request.POST.get('catoperation')
+        mangloc = request.POST.get('mangloc')
+        center110 = request.POST.get('center110')
+        center100 = request.POST.get('center100')
+        seprated_thin = request.POST.get('seprated_thin')
+        thin = models.ThinDevicesUnits(name=name, build=build, groupwork=groupwork,
+                                       twinte_five_percentgy=twinte_five_percentgy, fifty_percentgy=fifty_percentgy,
+                                       catoperation=catoperation
+                                       , mangloc=mangloc, center110=center110,
+                                       center100=center100, seprated_thin=seprated_thin)
+        thin.save()
+        if thin.pk:
+            return JsonResponse({"data": 1})
+        else:
+            return JsonResponse({"data": -1})
+
+
+def update_main(request):
+    if request.method == "POST" and request.is_ajax:
+        update_id = request.POST.get('update_id')
+        name = request.POST.get('name')
+        build = request.POST.get('build')
+        groupwork = request.POST.get('groupwork')
+        twinte_five_percentgy = request.POST.get('twinte_five_percentgy')
+        catoperation = request.POST.get('catoperation')
+        mangloc = request.POST.get('mangloc')
+        center110 = request.POST.get('center110')
+        center100 = request.POST.get('center100')
+        seprated_thin = request.POST.get('seprated_thin')
+        thin = models.ThinDevicesUnits.objects.get(pk=update_id)
+        thin.name = name
+        thin.build = build
+        thin.groupwork = groupwork
+        thin.twinte_five_percentgy = twinte_five_percentgy
+        thin.catoperation = catoperation
+        thin.mangloc = mangloc
+        thin.center110 = center110
+        thin.center100 = center100
+        thin.seprated_thin = seprated_thin
+        thin.save()
+        return JsonResponse({"data": 1})
+
+
+def MainReport(request):
+    if request.method == "POST":
+        status = request.POST.get('status')
+        name = request.POST.get('name')
+        template = get_template('main_report.html')
+        units = models.ThinDevicesUnits.objects.filter(name__contains=name, status=status)
+        user_obj = User.objects.get(pk=request.user.pk)
+        context = {
+            "company": "مركز النظم و الدعم المتكامل",
+            "user": user_obj,
+            "devices": units,
+            "topic": "تفاصيل العمل اليومى للوحدة  ",
+            "today": datetime.today().strftime('%Y-%m-%d'),
+        }
+        html = template.render(context)
+        pdf = render_to_pdf('main_report.html', context)
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Invoice_%s.pdf" % ("12341231")
+            content = "inline; filename='%s'" % (filename)
+            download = request.GET.get("download")
+            if download:
+                content = "attachment; filename='%s'" % (filename)
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse("Not found")
+    return render(request, 'devices/main_report_page.html')
+
+
+@login_required(login_url='login')
+def sara_grid(request):
+    uts = models.ThinDevicesUnits.NAME_CHOICES
+    qs = models.ThinDevicesUnits.objects.all()
+    clients_filter = ThinClientFilter(request.GET, queryset=qs)
+    thins = clients_filter.qs
+    page = request.GET.get('page', 1)
+    paginator = Paginator(thins, 8)
+    try:
+        thins = paginator.page(page)
+    except PageNotAnInteger:
+        thins = paginator.page(1)
+    except EmptyPage:
+        thins = paginator.page(paginator.num_pages)
+    return render(request, 'devices/device.html', context={"data": thins, "units": uts})
